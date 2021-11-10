80fa9a8cb60bb0f59d205e27a64abd8bfee230bd
yang <mianqiangyang@gmail.com>
2021-11-10 21:26:55 +0800

    submit 04

--- data structure/fourth/homewolk14.c
+++ data structure/fourth/homewolk14.c
@@ -4,6 +4,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(){
-    
+int main()
+{
+    int a[10][10];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d", a[i][j]);
+        }
+    }
+    //进行比较
+    //使用这种想法，直接选一个值，和后面的比较，有相同的就返回，没有就不返回
+
+    int j, i;
+    int target = a[0][0]; //定义一个量，去和二维数组中的元素进行比较
+    while (1)
+    {
+        //这个for循环走完才算是把第一次的数据比对完成。
+        for (i = 0; i < 10; i++)
+        {
+
+            for (j = 10 - i; j < 10; j++)
+            {
+                if (target == a[i][j])
+                {
+                    printf("二维数组中有元素相同\n");
+                    return 0;
+                }
+            }
+        }
+        
+    }
+    return 0;
 }
--- data structure/fourth/homewolk15.c
+++ data structure/fourth/homewolk15.c
@@ -1,3 +1,35 @@
 /**
- * 作业16 一个数组有n个整数，编写算法，将所有帧数排在所有复数前面，且书简复杂度为O(n)
+ * 作业16 一个数组有n个整数，编写算法，将所有正数排在所有负数前面，且时间复杂度为O(n)
  */
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+int main()
+{
+    int a[100];
+    printf("please input the array\n");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    /*
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    */
+  //  printf("\n");
+    //冒泡法排序，什么叫做时间复杂度为n，就是执行一次就可成功
+    int b = 0 , t = 0;
+    for(int i =0;i<5;i++){
+        if(a[i] >= 0) {
+            t = a[b];
+            a[b] = a[i];
+            a[i] = t;
+            b++;
+        }
+    }
+    for(int i = 0; i < 5; i++){
+        printf("%d ", a[i]);
+    }
+}
