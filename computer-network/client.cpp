18e6b970fd728b0ac5542df2491d713ad3737547
yang <mianqiangyang@gmail.com>
2021-12-27 15:43:23 +0800

    submit the comeputer-network

--- computer-network/client.cpp
+++ computer-network/client.cpp
@@ -1,31 +1,77 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<WinSock2.h>
-#pragma comment (lib,"ws2-32.lib")
+/*
+* 1：加载套接字库，创建套接字(WSAStartup()/socket())；
 
-int main(){
-    // 初始化DLL
+2：向服务器发出连接请求(connect())；
+
+3：和服务器端进行通信(send()/recv())；
+
+4：关闭套接字，关闭加载的套接字库(closesocket()/WSACleanup())。
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <WinSock2.h>
+// #pragma comment (lib,"ws2-32.lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable : 4996)
+using namespace std;
+
+int main()
+{
+    /*
+        WORD w_req = MAKEWORD(2,2);  // 版本号
+    */
+    // 初始化DLL  初始化套接字库
     WSADATA wsaDate;
-    WSAStartup(MAKEWORD(2,2), &wsaDate);
-    
+
+    WSAStartup(MAKEWORD(2, 2), &wsaDate);
+    /*
+    if (err != 0) {
+        cout << "初始化套接字库失败！" << endl;
+    }
+    else {
+        cout << "初始化套接字库成功！" << endl;
+    }
+    //检测版本号
+    if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wHighVersion) != 2) {
+        cout << "套接字库版本号不符！" << endl;
+        WSACleanup();
+    }
+    else {
+        cout << "套接字库版本正确！" << endl;
+    }
+    */
+    /*
+    这里没有判断判断初始化套接字库是否成功
+    以及检测套接字库的版本号
+    */
+
     //创建套接字
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    // 向服务器发送请求
+    /*
+    创建服务器端地址客户的地址
+    */
     struct sockaddr_in sockAddr;
+    // 先全部使用0来进行填充
     memset(&sockAddr, 0, sizeof(sockAddr));
     sockAddr.sin_family = AF_INET;
-    sockAddr.sin_addr.s_addr = inet_addr("AF_INET");
+    /*
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    */
+    sockAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
     sockAddr.sin_port = htons(1234);
-    connect(sock, (SOCKADDR*)&sockAddr,sizeof(SOCKADDR));
+    // 向服务器发送请求
+    connect(sock, (SOCKADDR *)&sockAddr, sizeof(SOCKADDR));
 
     //接受服务器传回来的数据
 
     char szBuffer[200] = {0};
-    recv(sock,szBuffer,200,NULL);
+    recv(sock, szBuffer, 200, NULL);
 
     //输出接收到的数据
-    printf("meeage for server : %s\n",szBuffer);
+    printf("meeage for server : %s\n", szBuffer);
 
     //关闭套接字
     closesocket(sock);
@@ -33,8 +79,5 @@ int main(){
     //终止使用DLL
     WSACleanup();
 
-    system("psuser");
     return 0;
-
-
 }
--- computer-network/server.cpp
+++ computer-network/server.cpp
@@ -1,40 +1,74 @@
+/*
+1：加载套接字库，创建套接字(WSAStartup()/socket())；
+
+2：绑定套接字到一个IP地址和一个端口上(bind())；
+
+3：将套接字设置为监听模式等待连接请求(listen())；
+
+4：请求到来后，接受连接请求，返回一个新的对应于此次连接的套接字(accept())；
+
+5：用返回的套接字和客户端进行通信(send()/recv())；
+
+6：返回，等待另一连接请求；
+
+7：关闭套接字，关闭加载的套接字库(closesocket()/WSACleanup())。
+*/
 #include<stdio.h>
+#include<iostream>
 #include<WinSock2.h>
-#pragma comment (lib,"ws2-32.lib")
+// #pragma comment (lib,"ws2-32.lib")
+#pragma comment (lib,"ws2_32.lib")
+using namespace std;
 
-int main(){
+int main() {
     // 初始化DLL
+    /*
+    WORD w_req = MAKEWORD(2,2)  // 版本号
+    WSADATA wsadata;
+    // 这个是来判断初始化的套接字库到底正不正确
+    int err;
+    err = WSAStartup(w_req,&wsadata)
+    */
     WSADATA wsaDate;
-    WSAStartup(MAKEWORD(2,2), &wsaDate);
-    
-    //创建套接字
-    SOCKET socket = socket(AF_INET, SOCK_STREAM, 0);// 0 ? IPPROTO_TCP
+    WSAStartup(MAKEWORD(2, 2), &wsaDate);
 
+    //创建套接字
+    SOCKET s_server = socket(AF_INET, SOCK_STREAM, 0);// 0 ? IPPROTO_TCP
+    SOCKET s_accept;
     // 绑定套接字
+    // 创建服务端的地址
     struct sockaddr_in sockAddr;
+    // 创建接收端的地址  已经被创建过了
+   //  struct sockaddr_in acceptAddr;
+    // 使用0填充
     memset(&sockAddr, 0, sizeof(sockAddr));
     sockAddr.sin_family = AF_INET;
-    sockAddr.sin_addr.s_addr = inet_addr(AF_INET); // 具体ip地址
-    sockAddr.sin_port = htons(1234) // 端口
-    bind(servSock,(struct sockaddr *)&sockAddr,sizeof(SOCKADDR));
+    /*
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 具体ip地址
+    */
+    //  看不懂
+    sockAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+    sockAddr.sin_port = htons(1234); // 端口
+    ::bind(s_server, (struct sockaddr*)&sockAddr, sizeof(SOCKADDR));
 
     // 进入监听状态
-    listen(servSock,20);
+    listen(s_server, 20);
 
     //接受客户端请求
     SOCKADDR clntAddr;
     int nSize = sizeof(SOCKADDR);
-    SOCKET clntSock = accept(servSock,(struct sockaddr *)&clntAddr,nSize);
+    // 这里创建了接收端
+    SOCKET clntSock = accept(s_server, (struct sockaddr*)&clntAddr, &nSize);
 
     // 向客户端发送信息
 
-    char *str = "Hello World";
-    send(clntSock,strlen(str)+sizeof(char), NULL);
+    char str[] = "Hello World";
+    send(clntSock, str, 100,0);
 
     //关闭套接字
 
     closesocket(clntSock);
-    closesocket(servSock);
+    closesocket(s_server);
 
     // 终止 DLL 使用
     WSACleanup();
