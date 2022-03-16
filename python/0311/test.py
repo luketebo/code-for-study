7b351ed8347186f15f06bdac1d3392edd0f1cf11
luke tebo <77975195+luketebo@users.noreply.github.com>
2022-03-16 23:24:50 +0800

    more

--- python/0311/test.py
+++ python/0311/test.py
@@ -2,6 +2,6 @@ import requests
 for i in range(100):
     print(f"正在爬去第{i}张图片")
     with open(f"{i:0>2d}.jpg","wb") as f:
-        r = requests.get("https://api.ixiaowai.cn/api/api.php")
+        r = requests.get("http://imgapi.xl0408.top/index.php")
         f.write(r.content)
         f.close()
---
+++ python/0315/demo.c++
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int count;
+    for(int i = 0; i < a; i++)
+    {
+        count *= i;
+    }
+   count << count << endl;
+}
