b11885a5dbc8a37f714c47cfc60c650c3f80be0e
luke tebo <77975195+luketebo@users.noreply.github.com>
2022-04-22 23:22:42 +0800

    😒😒😒😒😒

---
+++ python/project_home/0422/test.py
@@ -0,0 +1,71 @@
+from soupsieve import Dict
+
+
+voctionary = set()
+
+
+def show():
+    print(voctionary)
+    
+def add(key, value):
+    word = (key, value)
+    voctionary.add(word)
+    
+def remove():
+    key = input("请输入要删除的单词：")
+    value = input("请输入要删除的单词的意思：")
+    test = (key, value)
+    word = set()
+    word.add(test)
+    if word in voctionary:
+        voctionary.remove(word)
+        print("删除成功")
+    else:
+        print("删除失败")
+    
+def clear():
+    voctionary.clear()
+    
+def recite():
+    dic = {}
+    for item in voctionary:
+        print(item)
+        print(list(item))
+        dic.update(item[0], item[1])  
+    for key, value in dic.items():
+        answer = input(f"{key}的意思是：")
+        if answer == value:
+            print("回答正确")
+        else:
+            print("回答错误")
+    
+    
+def main():
+    print("欢迎使用生词记录器")
+    print("1. 查看生词本")
+    print("2. 背单词")
+    print("3. 添加生词")
+    print("4. 删除单词")
+    print("5. 清空单词本")
+    print("6. 退出生词记录器")
+    print("================")
+    while True:
+        num = int(input("请输入您的选择："))
+        if num == 1:
+            show()
+        elif num == 2:
+            recite()
+        elif num == 3:
+            key = input("请输入要添加的单词：")
+            value = input("请输入要添加的单词的意思：")
+            add(key, value)
+        elif num == 4:
+            remove()
+        elif num == 5:
+            clear()
+        elif num == 6:
+            break
+        else:
+            print("输入错误，请重新输入")
+            
+main()
