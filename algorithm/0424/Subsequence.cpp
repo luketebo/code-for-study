aed9c04a69d9c86092d05c16b30b335e7bde76c9
luketebo <mianqiangyang@gmail.com>
2022-04-24 19:25:09 +0800

    fuck vscode

---
+++ algorithm/0424/Subsequence.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file Subsequence.cpp
+ * @author Luke Tebo (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * 最长公共子序列问题
+ * 
+ * 输入：x <......> y <......>
+ * 输出：求一个公共子序列z <.....>
+ * 
+ */
+#include <iostream>
+#include <bits/stdc++.h>
+// 公共序列
+void LCS(vector<int>& num_01, vector<int>& num_02){
+    
+}
+
+int main(){
+
+    return 0;
+}
