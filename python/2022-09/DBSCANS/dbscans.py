087f857ca2337a786e619d15a6492d93d82f7756
luketebo <mianqiangyang@gmail.com>
2022-10-05 19:11:42 +0800

    submit fuck you

--- python/2022-09/DBSCANS/dbscans.py
+++ python/2022-09/DBSCANS/dbscans.py
@@ -1,2 +1,10 @@
 from sklearn.cluster import DBSCAN
 import numpy as np
+import matplotlib.pyplot as plt
+
+samples = np.loadtxt("./DBSACNS/kmeansSamples.txt")
+clustering = DBSCAN(eps=5, min_samples = 5).fit(samples)
+x = clustering.labels_
+print(x)
+plt.scatter(samples[:, 0], samples[:, 1], c=clustering.labels_ + 1.5, linewidths=np.power(clustering.labels_ + 1.5, 2))
+plt.show()
