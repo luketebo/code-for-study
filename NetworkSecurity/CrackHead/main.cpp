c7ec46604e4940cf4ef4c9b1f3016a45fc7caf22
luketebo <mianqiangyang@gmail.com>
2022-12-17 22:20:30 +0800

    today

---
+++ NetworkSecurity/CrackHead/main.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+using namespace std;
+// 生成注册码
+unsigned long int CrackHead()
+{
+    return 0;
+}
+// 获取系统卷标 并改为16进制
+unsigned long int getVolumeInformation()
+{
+    return 0;
+}
+int main()
+{
+    char a[20] = "";
+    cout << "输入卷标" << endl;
+    cin >> a;
+    cout << a << endl;
+
+    
+
+    return 0;
+}
