a78d27b63ceaadce4ccd48841006243d7e7de990
luketebo <mianqiangyang@gmail.com>
2023-01-26 21:43:46 +0800

    today things

--- web/大三上寒假/图像滑块/script.js
+++ web/大三上寒假/图像滑块/script.js
@@ -6,3 +6,16 @@ document.getElementById('prev').onclick = function (param) {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1])
 }
+
+document.onkeydown = f
+
+function f(e) {
+    // ArrowRight = 39 ArrowLeft = 37
+    if (e.keyCode == 39) {
+        let lists = document.querySelectorAll('.item');
+        document.getElementById('slide').appendChild(lists[0]);
+    } else if (e.keyCode == 37) {
+        let lists = document.querySelectorAll('.item');
+        document.getElementById('slide').prepend(lists[lists.length - 1]);
+    }
+}
